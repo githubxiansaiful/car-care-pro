5749a93c313d74dbc4e7438e0d06c99770a1f535
Xian Saiful <xiansaiful@gmail.com>
2026-01-07 05:35:23 +0600

    Added Nav

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Footer = () => {
     return (
-        <footer class="bg-[#f1f1f1] pt-[50px]">
+        <footer className="bg-[#f1f1f1] pt-[50px]">
             <div className="container">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
                     <div className="space-y-5">
@@ -11,7 +11,7 @@ const Footer = () => {
                     </div>
                     <div className="space-y-5">
                         <h3 className="font-bold uppercase">Quick Links</h3>
-                        <ul>
+                        <ul className="space-y-2">
                             <li><a href="#">Home</a></li>
                             <li><a href="#">Services</a></li>
                             <li><a href="#">About Us</a></li>
@@ -20,7 +20,7 @@ const Footer = () => {
                     </div>
                     <div className="space-y-5">
                         <h3 className="font-bold uppercase">Contact Us</h3>
-                        <ul>
+                        <ul className="space-y-2">
                             <li><p>1234 Auto Lane</p></li>
                             <li><p>Car City, CA 90210</p></li>
                             <li><p>Phone: (123) 456-7890</p></li>
@@ -29,7 +29,7 @@ const Footer = () => {
                     </div>
                     <div className="space-y-5">
                         <h3 className="font-bold uppercase">Follow Us</h3>
-                        <ul>
+                        <ul className="space-y-2">
                             <li><a href="#">Facebook</a></li>
                             <li><a href="#">Twitter</a></li>
                             <li><a href="#">Instagram</a></li>
