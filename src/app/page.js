c8bd09837e430696bf31cebe5a109d8015658313
Xian Saiful <xiansaiful@gmail.com>
2026-01-07 06:49:00 +0600

    Added Services component to Home page and implemented service listing with images and details.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroBanner from "./ui/home/HeroBanner";
+import Services from "./ui/home/Services";
 
 export default function Home() {
   return (
     <div>
       <HeroBanner></HeroBanner>
+      <Services></Services>
     </div>
   );
 }
